5a4044a89b982cae0bf50a34cc60eb3a0e953381
Kwinten Van den Berghe <kwintenvdberghe@gmail.com>
2023-05-05 23:10:44 +0200

    Proxy API requests

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	// "context"
-	"io"
-	"net/http"
-	// "net/url"
 	"os"
 
-	// "github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/proxy"
@@ -20,50 +15,16 @@ import (
 // API gateway should first check the cache, otherwise the API will be called
 
 func main() {
-	// rdb := redis.NewClient(&redis.Options{
-	// 	Addr:     "localhost:6379",
-	// 	Password: "", // no password set
-	// 	DB:       0,  // use default DB
-	// })
-	// println("Connected to Redis")
-
-	// ctx := context.Background()
-	// rdb.Set(ctx, "my_key", "my_value", 0)
-	
-	// value := rdb.Get(ctx, "my_key").Val()
-	// println(value)
-
-	// TODO proxy middleware?
 	app := fiber.New()
-	// app.Post("/authenticate", authenticate)
 	app.Post("/authenticate", proxy.Forward(createApiServiceUrl("/authenticate")))
-	app.Get("/sales/:month", fetchSales)
-
-	// sales := fetchSales()
-	// rdb.Set(ctx, "sales/202210", sales, 0)
-
-	// s := rdb.Get(ctx, "sales/202210").Val()
-	// println(s)
+	app.All("/api/*", func(c *fiber.Ctx) error {
+		path := c.Path()
+		url := createApiServiceUrl(path)
+		return proxy.Do(c, url)
+	})
 
 	app.Use(logger.New())
 	app.Listen(":8080")
-}
-
-func fetchSales(c *fiber.Ctx) error {
-	
-	println("request path: " + c.Path())
-	res, err := http.Get(createApiServiceUrl(c.Path()))
-	if err != nil {
-		return err
-	}
-
-	defer res.Body.Close()
-	sales, err := io.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-	c.Send(sales)
-	return nil
 }
 
 func createApiServiceUrl(path string) string {
